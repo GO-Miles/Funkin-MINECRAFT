7724d6dbc43e4a963da13293f9f47b9fed685366
GO_Miles <itzmilesdev@gmail.com>
2025-11-19 14:22:01 -0700

    there will be a GameMenu

---
+++ source/menus/GameMenu.hx
@@ -0,0 +1,19 @@
+package menus;
+
+class GameMenu extends Menu
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function refresh()
+	{
+
+	}
+
+	override public function update(elapsed:Float)
+	{
+		
+	}
+}
